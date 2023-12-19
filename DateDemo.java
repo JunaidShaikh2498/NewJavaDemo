a9ad5201b2d6016cb04094adcd99b6407b2f5e25
JunaidShaikh2498 <92734752+JunaidShaikh2498@users.noreply.github.com>
2023-12-19 08:34:07 +0530

    Create DateDemo.java
    First git file

---
+++ DateDemo.java
@@ -0,0 +1,22 @@
+public class Date
+{
+    private int d,m,y;
+
+    public Date(){}
+    public Date(int d,int m,int y){
+        this.d=d;
+        this.m=m;
+        this.y=y;
+    }
+    public Date(Date d){
+        this.d=d.d;
+        this.m=d.m;
+        this.y=d.y;
+    }
+    public void showDate(){
+        System.out.println(d + "/" + m + "/" + y);
+    }
+    public void showDate(char c){
+        System.out.println(d+""+c+m+""+c+y);
+    }
+}
